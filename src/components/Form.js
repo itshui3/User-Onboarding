18934500df027c4499b71a391573684ae32d954a
Hui <jeffrey.x.hui@gmail.com>
2019-11-14 14:15:02 -0800

    added yup validation

--- src/components/Form.js
+++ src/components/Form.js
@@ -11,8 +11,11 @@ const InputForm = ({values, errors, touched }) => {
   return(
     <Form>
       <Field type="text" name="name" placeholder="name" />
+      {touched.name && errors.name && (<p>{errors.name}</p>)}
       <Field type="email" name="email" placeholder="email" />
+      {touched.email && errors.email && (<p>{errors.email}</p>)}
       <Field type="text" name="password" placeholder="password" />
+      {touched.password && errors.password && (<p>{errors.password}</p>)}
       <Field type="checkbox" name="tos" value={values.tos} />
 
       <button>Submit!</button>
@@ -32,10 +35,27 @@ const FormikInputForm = withFormik({
   },
 
   validationSchema: Yup.object().shape({
-    name: Yup.string().required(),
-    email: Yup.string().required(),
-    password: Yup.string().required()
-  })
+    name: Yup.string("Name needs to be a string")
+      .min(6, "Name needs to be at least 6 characters")
+      .required("Name required"),
+    email: Yup.string()
+      .required("Email required"),
+    password: Yup.string()
+      .required("Password required")
+  }),
+
+  handleSubmit(values, { setStatus }) {
+
+    axios
+      .post("https://reqres.in/api/users/", values)
+      .then(res => {
+        setStatus(res.data);
+        console.log(res);
+      })
+      .catch(err => {
+        console.log(err.response);
+      });
+  }
 })(InputForm);
 
 export default FormikInputForm;
